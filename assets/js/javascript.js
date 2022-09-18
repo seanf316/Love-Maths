033856a30d1391d5d3f16692c20cbb0026372d96
seanf316 <seanfinn316@gmail.com>
2022-09-18 20:10:24 +0000

    Setup structure for game functions

---
+++ assets/js/javascript.js
@@ -0,0 +1,31 @@
+function runGame () {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
+
+function displayDivideQuestion() {
+
+}
